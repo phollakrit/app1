f0f6677a32d7e7e881eb73f68ce698005e601537
pat123 <phollakrit@hotmail.com>
2023-08-18 12:45:25 +0700

    Revert "Create class-components.js"
    This reverts commit b24ec57f73c0409a96aa10c83e6c3d4a82cad932.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Content, Footer, Header } from './func-components';
-import Calendar from './class-components';
 
 function App() {
   return (
     <>
       <Header/>
-      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
       <div>React & React Native</div>
