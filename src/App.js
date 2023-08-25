e75bc799e15cdc2c01048674272e67b43765b9fa
pat123 <phollakrit@hotmail.com>
2023-08-25 15:41:35 +0700

    Create state-func.js
    Create state-func.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import RefsArray from './refs-array';
+import MessageBox from './state-func';
 
 function App() {
   return (
-    <RefsArray/>
+    <MessageBox/>
   )
 }
 
